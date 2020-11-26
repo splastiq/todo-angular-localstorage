4c58ce8d6a39da7f7c471f219cb0de42f4182ab2
Max Khorsun <max.khorsun@gmail.com>
2020-11-27 01:50:26 +0300

    feat: add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { DataService } from "./data.service";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
   providers: [DataService]
